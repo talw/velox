1e0e67e93f74b5395707206d2b50fc7742689f50
Alois Cochard <alois.cochard@gmail.com>
2014-07-25 16:48:47 +0100

    WIP

--- src/Velox/Job.hs
+++ src/Velox/Job.hs
@@ -15,10 +15,26 @@ import qualified Data.Map.Strict as M
 
 import Velox.Artifact (ArtifactId(..))
 import Velox.Dependencies (Dependencies, directDependencies, filterDependencies, reverseDependencies)
-import Velox.Job.Task (TaskContext(..), runTask, terminateTaskContext)
-import Velox.Project (prjId)
+import Velox.Job.Task (Task(..), TaskContext(..), ArtifactAction, ProjectAction, runArtifactTasks, terminateTaskContext)
+import Velox.Project (ProjectId, prjId)
 
-data Job = Job { jobTasks :: Map ArtifactId [FilePath] }
+data Job = Job { jobTasks :: [Task] }
+
+data Plan = Plan
+  { planProjectActions  :: [[(ProjectId,  [ProjectAction] )]]
+  , planArtifactActions :: [[(ArtifactId, [ArtifactAction])]] }
+
+artifactTasks :: [Task] -> Map ArtifactId [ArtifactAction]
+artifactTasks tasks = M.fromListWith (++) xs where
+  xs = tasks >>= \task -> case task of
+    ArtifactTask i a  -> [(i, [a])]
+    _                 -> []
+
+projectTasks :: [Task] -> Map ProjectId [ProjectAction]
+projectTasks tasks = M.fromListWith (++) xs where
+  xs = tasks >>= \task -> case task of
+    ProjectTask i a   -> [(i, [a])]
+    _                 -> []
 
 planArtifacts :: Dependencies -> [ArtifactId] -> [[ArtifactId]]
 planArtifacts ds [] = []
@@ -33,15 +49,19 @@ planArtifacts ds xs = case zs of
       directDeps = directDependencies deps
       deps = filterDependencies xs ds
 
-planJob :: Dependencies -> Job ->  [[(ArtifactId, [FilePath])]]
-planJob ds job = f <$> planArtifacts ds artifactIds where
-    f = (\xs -> (\x -> (x, M.findWithDefault [] x tasks)) <$> xs)
-    artifactIds = resolve $ M.keys tasks where
+
+planJob :: Dependencies -> Job -> Plan
+planJob ds job = Plan undefined $ f <$> planArtifacts ds artifactIds where
+    f = (\xs -> (\x -> (x, M.findWithDefault [] x artifactTasks')) <$> xs)
+    artifactIds = resolve $ M.keys artifactTasks' where
       resolve xs  = if zs == xs then xs else resolve zs where
         zs = L.nub (ys ++ xs)
         ys = (\x -> M.findWithDefault [] x reverseDeps) =<< xs
       reverseDeps = reverseDependencies ds
-    tasks       = jobTasks job
+    artifactTasks' :: Map ArtifactId [ArtifactAction]
+    artifactTasks' = artifactTasks $ jobTasks job
+    projectTasks' :: Map ProjectId [ProjectAction]
+    projectTasks' = projectTasks $ jobTasks job
 
 runJob :: Dependencies -> Job -> IO ()
 runJob ds j = do
@@ -57,14 +77,14 @@ runJob ds j = do
   where
     tryRun :: MVar TaskContext -> IO (Either AsyncException ())
     tryRun tcv = try $ do
-      success         <- foldM (runStep tcv) True $ planJob ds j
+      success         <- foldM (runStep tcv) True $ planArtifactActions $ planJob ds j
       putStrLn $ "(finish) " ++ show success where
         runStep tcv success xs = do
           swapMVar tcv $ TaskContext [] []
           case success of
             False -> return False
             True  -> do
-              asyncs <- traverse (createAsync tcv . runTask) xs
+              asyncs <- traverse (createAsync tcv . runArtifactTasks) xs
               xs <- traverse wait asyncs
               return $ and xs where
                 createAsync :: MVar TaskContext -> IO a -> IO (Async a)
--- src/Velox/Job/Task.hs
+++ src/Velox/Job/Task.hs
@@ -5,16 +5,39 @@ import Data.Traversable (traverse)
 import System.Process (ProcessHandle, terminateProcess)
 
 import Velox.Artifact (ArtifactId)
+import Velox.Project (ProjectId)
 
 -- TODO Remove
 import Control.Concurrent (threadDelay)
 
-runTask :: (ArtifactId, [FilePath]) -> IO Bool
-runTask (a, fps) = do
+data Task
+  = ArtifactTask  ArtifactId  ArtifactAction
+  | ProjectTask   ProjectId   ProjectAction
+  deriving (Eq, Show)
+
+data ArtifactAction
+  = TypeCheck [FilePath]
+  | Build
+  deriving (Eq, Ord, Show)
+
+data ProjectAction
+  = Configure
+  deriving (Eq, Ord, Show)
+
+data Action
+  = Success
+  | Failure String
+
+-- TODO Optimize actions!
+runArtifactTasks :: (ArtifactId, [ArtifactAction]) -> IO Bool
+runArtifactTasks (a, fps) = do
   putStrLn (show a ++ " START")
   threadDelay $ 1000 * 1000
   putStrLn (show a ++ " FINISH")
   return True
+
+runProjectTasks :: (ProjectId, [ProjectAction]) -> IO Bool
+runProjectTasks = undefined
 
 data TaskContext = TaskContext
   { asyncs    :: [Async ()]
